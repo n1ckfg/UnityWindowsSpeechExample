90b0164dca2e1b64948b2525d443290afa0859d8
Nick Fox-Gieg <nick@fox-gieg.com>
2019-08-15 23:33:01 -0400

    first

---
+++ Assets/Scripts/SpeechControl.cs
@@ -0,0 +1,34 @@
+﻿// https://developer.microsoft.com/en-us/windows/mixed-reality/voice_input_in_unity
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+public class SpeechControl : MonoBehaviour {
+
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+    void Start () {
+        //Create keywords for keyword recognizer
+        keywords.Add("blue", () => {
+            // action to be performed when this keyword is spoken
+        });
+
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args) {
+        System.Action keywordAction;
+        // if the keyword recognized is in our dictionary, call that Action.
+        if (keywords.TryGetValue(args.text, out keywordAction)) {
+            keywordAction.Invoke();
+        }
+    }
+
+}
