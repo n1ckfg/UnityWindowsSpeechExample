e41e304317ca4f6c1fb10f44b9daab8128bd195d
Nick Fox-Gieg <nick@fox-gieg.com>
2019-08-16 10:55:58 -0400

    refactor

---
+++ Assets/Scripts/SpeechKeyword.cs
@@ -0,0 +1,54 @@
+﻿// https://developer.microsoft.com/en-us/windows/mixed-reality/voice_input_in_unity
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+public class SpeechKeyword : MonoBehaviour {
+
+    public enum ConfidenceLevel { HIGH, MEDIUM, LOW };
+    public ConfidenceLevel confidenceLevel = ConfidenceLevel.MEDIUM;
+    public AudioSource sound;
+    public bool initOnStart = false;
+
+    [HideInInspector] public Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    private KeywordRecognizer keywordRecognizer;
+    
+    private void Start() {
+        if (initOnStart) {
+            setupKeywords();
+        }
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args) {
+        System.Action keywordAction;
+
+        sound.Play();
+
+        Debug.Log("Heard keyword: \"" + args.text + "\"");
+
+        if (keywords.TryGetValue(args.text, out keywordAction)) { // if the keyword recognized is in our dictionary, call that Action.
+            keywordAction.Invoke();
+        }
+    }
+
+    public void setupKeywords() {
+        keywords.Clear();
+
+        keywords.Add("red", () => { // Create keyword for keyword recognizer
+            // action to be performed when this keyword is spoken
+        });
+
+        init();
+    }
+    
+    public void init() {
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray(), (UnityEngine.Windows.Speech.ConfidenceLevel) confidenceLevel);
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+    
+}
---
+++ Assets/Scripts/SpeechKeyword_ColorChanger.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechKeyword_ColorChanger : MonoBehaviour
+{
+
+    public SpeechKeyword speech;
+
+    private Material mat;
+
+    private void Awake() {
+        mat = GetComponent<Renderer>().sharedMaterial;
+    }
+
+    private void Start() {
+        mat.SetColor("_Color", new Color(0.5f, 0.5f, 0.5f));
+
+        setupKeywords();
+    }
+
+    private void setupKeywords() {
+        speech.keywords.Clear();
+
+        speech.keywords.Add("red", () => {
+            mat.SetColor("_Color", new Color(1f, 0f, 0f));
+        });
+
+        speech.keywords.Add("green", () => {
+            mat.SetColor("_Color", new Color(0f, 1f, 0f));
+        });
+
+        speech.keywords.Add("blue", () => {
+            mat.SetColor("_Color", new Color(0f, 0f, 1f));
+        });
+
+        speech.keywords.Add("orange", () => {
+            mat.SetColor("_Color", new Color(1f, 0.5f, 0f));
+        });
+
+        speech.keywords.Add("yellow", () => {
+            mat.SetColor("_Color", new Color(1f, 1f, 0f));
+        });
+
+        speech.keywords.Add("pink", () => {
+            mat.SetColor("_Color", new Color(1f, 0f, 1f));
+        });
+
+        speech.keywords.Add("purple", () => {
+            mat.SetColor("_Color", new Color(0.5f, 0f, 1f));
+        });
+
+        speech.keywords.Add("teal", () => {
+            mat.SetColor("_Color", new Color(0f, 1f, 1f));
+        });
+
+        speech.keywords.Add("cyan", () => {
+            mat.SetColor("_Color", new Color(0f, 1f, 1f));
+        });
+
+        speech.keywords.Add("white", () => {
+            mat.SetColor("_Color", new Color(1f, 1f, 1f));
+        });
+
+        speech.keywords.Add("black", () => {
+            mat.SetColor("_Color", new Color(0f, 0f, 0f));
+        });
+
+        speech.keywords.Add("gray", () => {
+            mat.SetColor("_Color", new Color(0.5f, 0.5f, 0.5f));
+        });
+
+        speech.init();
+    }
+
+}
